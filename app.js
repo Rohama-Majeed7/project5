86436c47a9851b17487ec72b4e7f7dffbd92fb6f
Rohama-Majeed7 <139218403+Rohama-Majeed7@users.noreply.github.com>
2023-12-03 02:20:57 +0500

    chnge

--- app.js
+++ app.js
@@ -4,23 +4,23 @@ btn.addEventListener("click", () => {
   showbar.classList.toggle("show-sidebar");
 });
 // ================================ //
-var mybutton = document.querySelector(".icon");
+const links = document.querySelectorAll(".nav-link li a");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
+// Loop through each anchor link
+links.forEach((link) => {
+  // Listen for click event
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-// When the user clicks on the button, scroll to the top of the document
-function scrollToTop() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
-}
-// ==========================//
+    const targetId = this.textContent.toLowerCase();
+
+    const targetSection = document.getElementById(targetId);
+
+    const offsetTop = targetSection.offsetTop;
+
+    window.scrollTo({
+      top: offsetTop,
+      behavior: "smooth",
+    });
+  });
+});
