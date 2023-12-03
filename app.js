bf9c7d4302af4a3a32fdb133bec083ebbf3642ff
Rohama-Majeed7 <139218403+Rohama-Majeed7@users.noreply.github.com>
2023-12-03 23:39:44 +0500

    change

--- app.js
+++ app.js
@@ -24,3 +24,39 @@ links.forEach((link) => {
     });
   });
 });
+// ============================= //
+ScrollReveal({
+  reset: true,
+  ditance: "60px",
+  duration: 2500,
+  easing: "ease-in-out",
+});
+ScrollReveal().reveal(".about-info", { delay: 100, origin: "right" });
+ScrollReveal().reveal(".about-img", { delay: 100, origin: "left" });
+// ========================= //
+document.addEventListener("DOMContentLoaded", function () {
+  const images = [
+    "./images/hero.jpg",
+    "./images/a.jpg",
+    "./images/b.jpg",
+    "./images/c.jpg",
+    "./images/d.jpg",
+  ]; // Add more image URLs as needed
+  const hero = document.querySelector(".hero");
+  let currentIndex = 0;
+
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateBackground();
+  }
+
+  function updateBackground() {
+    hero.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.721), rgba(0, 0, 0, 0.455)),
+   url(${images[currentIndex]})`;
+  }
+
+  hero.addEventListener("transitionend", function () {
+    hero.style.backgroundImage = `url(${images[currentIndex]})`;
+  });
+  setInterval(nextSlide, 6000); // Change background every 3 seconds (adjust as needed)
+});
