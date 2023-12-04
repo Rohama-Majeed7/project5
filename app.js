eaa6082a2f7ad69529636701c29227470c0e5095
Rohama-Majeed7 <139218403+Rohama-Majeed7@users.noreply.github.com>
2023-12-04 10:08:57 +0500

    change

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const btn = document.querySelector(".btn");
 const showbar = document.querySelector(".side-bar");
+const cancel = document.querySelector(".cancel");
 btn.addEventListener("click", () => {
-  showbar.classList.toggle("show-sidebar");
+  showbar.classList.add("show-sidebar");
+});
+cancel.addEventListener("click", () => {
+  showbar.classList.remove("show-sidebar");
 });
 // ================================ //
 const links = document.querySelectorAll(".nav-link li a");
@@ -25,35 +29,3 @@ links.forEach((link) => {
   });
 });
 // ============================= //
-ScrollReveal({
-  reset: true,
-  ditance: "60px",
-  duration: 2500,
-  easing: "ease-in-out",
-});
-ScrollReveal().reveal(".about-info", { delay: 100 }, { origin: "right" });
-ScrollReveal().reveal(".about-img", { delay: 100 }, { origin: "left" });
-// ========================= //
-document.addEventListener("DOMContentLoaded", function () {
-  const images = [
-    "./images/hero.jpg",
-    "./images/skyline2.jpg",
-    "./images/skyline3.jpg",
-  ]; // Add more image URLs as needed
-  const hero = document.querySelector(".hero");
-  let currentIndex = 0;
-
-  function nextSlide() {
-    currentIndex = (currentIndex + 1) % images.length;
-    updateBackground();
-  }
-
-  function updateBackground() {
-    hero.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.721), rgba(0, 0, 0, 0.455)), url(${images[currentIndex]})`;
-    hero.style.backgroundPosition = "0% 0%";
-    void hero.offsetWidth;
-    hero.style.backgroundPosition = "50% 50%";
-  }
-
-  setInterval(nextSlide, 6000);
-});
